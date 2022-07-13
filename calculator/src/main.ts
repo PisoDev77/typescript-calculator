5a2f753bb8933e88a29a539d9495da9c4f970cb3
PisoDev77 <pisodev0427@gmail.com>
2022-07-13 17:26:16 +0900

    계산기 렌더링 구현 // this 에러 발견

--- calculator/src/main.ts
+++ calculator/src/main.ts
@@ -3,24 +3,38 @@ import "./style.css";
 const VALID_NUMBER_OF_DIGITS = 3;
 const BASE_DIGIT = 10;
 
-//1. HTML 요소 타이핑
-// assertion 타입 단언에 대한 내용 숙지 필요
-// document.querySelector("#result").innerText = 9999;
-// const resultEl = document.querySelector("#result") as HTMLElement;
-const resultEl = <HTMLElement>document.querySelector("#result");
 
-if(resultEl){
-    resultEl.innerText = String(99999);
+
+const Calcultor = {
+    value: 0,
+
+    render(){//결과 값을 계속 바꾸기
+
+        const resultEl = <HTMLElement>document.querySelector("#result");
+
+        if(resultEl){
+            resultEl.innerText = String(this.value);
+        }
+    },
+    reset(){
+        this.value = 0;
+    },
+    initEvent(){
+       
+        const buttonContainerEl = document.querySelector(".contents");
+    
+        buttonContainerEl?.addEventListener('click', function({target}){
+            const buttonText = (target as HTMLButtonElement).innerText;
+    
+            if(buttonText === "AC"){
+                this.reset(); // this의 외부 값은 섀도 처리됨.
+            } else{
+    
+            }
+        });
+        
+    }
 }
 
-//런타임 환경에서 나올 수 있는 에러 탐지함.
-/*
-resultEl.addEventListener("click", ({target})=> {
-    alert(target.innerText);
-})
-*/
-resultEl.addEventListener("click", ({target}: MouseEvent)=> {
-    if(target){
-        alert((target as HTMLDivElement).innerText);
-    }
-});
+Calcultor.render();
+Calcultor.initEvent();
