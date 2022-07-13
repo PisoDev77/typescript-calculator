97496def207d032c50e77fbdde2006fb9f99fac0
PisoDev77 <pisodev0427@gmail.com>
2022-07-13 17:46:51 +0900

    복수 숫자 입력

--- calculator/src/main.ts
+++ calculator/src/main.ts
@@ -2,10 +2,13 @@ import "./style.css";
 
 const VALID_NUMBER_OF_DIGITS = 3;
 const BASE_DIGIT = 10;
+const INIT_VALUE = 0;
 
 const validateNumberLength = (value: string | number)=>{
     return String(value).length < VALID_NUMBER_OF_DIGITS;
 };
+
+const isZero = (value: string) => Number(value) === 0;
 
 const Calcultor = {
     value: 0,
@@ -13,17 +16,20 @@ const Calcultor = {
     render(inputValue: string | number ){//결과 값을 계속 바꾸기
 
         const resultEl = <HTMLElement>document.querySelector("#result");
-        const prevValue = Number(resultEl.innerText);
+        const prevValue = resultEl.innerText;
+
+        if(validateNumberLength(prevValue)){
+            return;
+        }
 
         if(resultEl){
-            resultEl.innerText = isNaN(prevValue) ? String(inputValue) : String(prevValue)
+            resultEl.innerText =
+            isZero(prevValue)? 
+            String(inputValue) : String(prevValue + inputValue)
         }
     },
     reset(){
         this.value = 0;
-    },
-    operator(inputNumber: number){
-        this.render(inputNumber)
     },
     initEvent(){
        
@@ -38,12 +44,12 @@ const Calcultor = {
                 //2. Arrow 함수 활용 this를 알 수 있으니까
                 this.reset(); // this의 외부 값은 섀도 처리됨.
             } else{
-                this.operator(Number(buttonText));
+                this.render(Number(buttonText));
             }
         });
         
     }
 }
 
-Calcultor.render();
+Calcultor.render(INIT_VALUE);
 Calcultor.initEvent();
